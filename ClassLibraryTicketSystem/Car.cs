921df6e667eab428e265d715b516afcd481516d8
AlexandruLazaroiu <alexandru_lazaroiu94@yahoo.com>
2021-09-30 05:56:55 +0200

    Add project files.

---
+++ ClassLibraryTicketSystem/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{   /// <summary>
+    /// Car class
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// Constructor for Car class with 2 parameters
+        /// </summary>
+        /// <param name="licensePlate"></param>
+        /// <param name="Date"></param>
+        public Car(string licensePlate, DateTime Date)
+        {
+
+        }
+        /// <summary>
+        /// base price
+        /// </summary>
+        /// <returns>double 240</returns>
+        public double Price()
+        {
+            return 240;
+        }
+        /// <summary>
+        /// A string returning the type of car.
+        /// </summary>
+        /// <returns></returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+
+
+}
---
+++ ClassLibraryTicketSystemTests/CarTests.cs
@@ -0,0 +1,44 @@
+﻿using ClassLibraryTicketSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void CarTest()
+        {
+            //arrange
+
+            //act
+
+            //assert
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            //arrange
+            
+
+            //act
+            
+
+            //assert
+            Assert.Fail();
+        }
+    }
+}
